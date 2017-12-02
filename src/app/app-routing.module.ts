f2c256a6b78590def0cc14c992a967a37366ba37
Whitney <whitneyrabbitc@gmail.com>
2017-12-02 18:43:28 +0100

    re-ordered

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
+import { EventComponent } from "./event/event.component";
+import { EventStartComponent } from "./event/event-start/event-start.component";
 import { EventDetailComponent } from './event/event-detail/event-detail.component';
-import {EventComponent} from "./event/event.component";
-import {EventStartComponent} from "./event/event-start/event-start.component";
-import {EventNewComponent} from "./event/event-new/event-new.component";
+import { EventNewComponent } from "./event/event-new/event-new.component";
 
 const appRoutes: Routes = [
   {path: '', component: EventComponent, pathMatch: 'full', children: [
-  {path: '', component: EventStartComponent},
-  {path: ':id', component: EventDetailComponent}
+  // {path: '', component: EventStartComponent},
+  {path: '', component: EventDetailComponent}
 ]},
   {path: 'newevent', component: EventNewComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import {EventItemComponent} from './event/event-list/event-item/event-item.component';
+import { EventComponent } from "./event/event.component";
 import { EventListComponent } from './event/event-list/event-list.component';
-import {EventService} from "./event/event.service";
-import {AppRoutingModule} from "./app-routing.module";
-import {HttpModule} from "@angular/http";
 import { EventDetailComponent } from './event/event-detail/event-detail.component';
-import {EventComponent} from "./event/event.component";
+import { EventItemComponent } from './event/event-list/event-item/event-item.component';
 import { EventStartComponent } from './event/event-start/event-start.component';
 import { EventNewComponent } from './event/event-new/event-new.component';
-import {FormsModule} from "@angular/forms";
+
+import { EventService } from "./event/event.service";
+import { AppRoutingModule } from "./app-routing.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    EventListComponent,
-    EventItemComponent,
     EventComponent,
+    EventListComponent,
     EventDetailComponent,
+    EventItemComponent,
     EventStartComponent,
     EventNewComponent
 
