1a688ab5d35cad0e76757affdabdbe71b82053de
Whitney <whitneyrabbitc@gmail.com>
2017-12-14 12:05:46 +0100

    edited own directive

--- src/app/directives/color.directive.ts
+++ src/app/directives/color.directive.ts
@@ -7,10 +7,15 @@ export class ColorDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    const button = document.getElementsByTagName('button');
+    document.getElementById('btn').style.color = "#fff";
+    document.getElementById('btn').style.borderRadius= "4px";
+    document.getElementById('btn').style.border= "1px solid transparent";
+    document.getElementById('btn').style.padding= "6px 12px";
     this.backgroundColor = this.defaultColor;
   }
-  @Input() defaultColor: string = 'transparent';
-  @Input('colorDir') highlightColor: string = 'blue';
+  @Input() defaultColor: string = '#5cb85c';
+  @Input('colorDir') highlightColor: string = '#449d44';
 
   @HostBinding('style.backgroundColor') backgroundColor: string;
   @HostListener('mouseover') mouseover(eventData: Event) {
